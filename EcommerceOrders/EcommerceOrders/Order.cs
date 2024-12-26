fd995b133180c9b2e3ac185c9259bb627e5aea61
BalmaneAyoub <ayoub.balmane@edu.itspiemonte.it>
2024-12-26 11:47:58 +0100

    creazione classe order
    da cui ricavo il totale e il totale senza sconto

---
+++ EcommerceOrders/EcommerceOrders/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceOrders
+{
+    public class Order
+    {
+        public int id { get; set; }
+        public string article { get; set; }
+        public int quantity { get; set; }
+        public decimal unitprice { get; set; }
+        public decimal percentagediscount { get; set; }
+        public string buyer { get; set; }
+
+        public decimal Total()
+        {
+            return quantity * unitprice * (1- percentagediscount/100);
+        }
+
+        public decimal Totalnodiscount() 
+        {    
+            return quantity * unitprice;
+        }
+    }
+}
